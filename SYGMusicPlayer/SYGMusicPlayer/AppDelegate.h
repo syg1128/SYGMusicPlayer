39fb973feb8f3ce431de70a924f4298b1e9840df
SunYanGui <1045436827@qq.com>
2018-10-09 00:49:56 +0800

    [CHG][Add]创建项目工程

---
+++ SYGMusicPlayer/SYGMusicPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SYGMusicPlayer
+//
+//  Created by SunYanGui on 2018/10/9.
+//  Copyright © 2018年 SunYanGui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SYGMusicPlayer/SYGMusicPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SYGMusicPlayer
+//
+//  Created by SunYanGui on 2018/10/9.
+//  Copyright © 2018年 SunYanGui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
